50f49726bc9b2d6c894b58347234cd3b80485fee
John_Luke <147130465+John-Luke-7@users.noreply.github.com>
2024-04-13 23:05:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,50 +7,36 @@ const client = new Discord.Client({
 const keepAlive = require('./server.js');
 keepAlive();
 
-function formatTime() { //Credits to himika#0001 and never#0001
-  const date = new Date();
-  const options = {
-    timeZone: 'Asia/Kolkata', //https://www.zeitverschiebung.net/en/ and find your city and enter here
-    hour12: true,
-    hour: 'numeric',
-    minute: 'numeric'
-  };
-  return new Intl.DateTimeFormat('en-US', options).format(date);
-}
-
 client.on('ready', async () => {
   console.clear();
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
     .setApplicationId('1154459332914720878')
-    .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Playing AS : Duggles_D_Xebec')
-    .setName('In The Moon')
-    .setDetails(`Blood Bleeding , Kidney 🍒[${formatTime()}]`)
+    .setType('PLAYING')
+    .setURL('https://twitch.tv/discord')
+    .setState('Team MGC')
+
+    .setName('Blood Bleeding KidneY')
+    .setDetails('Playing As Duggles_D_Xebec')
+    .setParty({
+        max: 9999,
+        current: 6789,
+        id: Discord.getUUID(),
+    })
+
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/external/g0dRFUr3eL4xv76GNWWUjAT9c2MEkXWtxGkumpjbHeU/https/i.pinimg.com/originals/19/f7/e8/19f7e88ecdf3ead830ee96a82e3de21a.gif') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Team MGC') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/external/eK_s96YiVIfvG14xk6TFz4ZQWuVTRi0M_I7oPZhpoto/https/emoji.discadia.com/emojis/798267a5-3e6c-4168-a56c-9fc4be65aad3.gif') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Hypton') //Text when you hover the Small image
-    .addButton('JOIN KMRP', 'https://dsc.gg/kurukkanmoola')
+    .setAssetsLargeImage('https://media.discordapp.net/external/g0dRFUr3eL4xv76GNWWUjAT9c2MEkXWtxGkumpjbHeU/https/i.pinimg.com/originals/19/f7/e8/19f7e88ecdf3ead830ee96a82e3de21a.gif')
+    .setAssetsSmallImage('https://media.discordapp.net/external/eK_s96YiVIfvG14xk6TFz4ZQWuVTRi0M_I7oPZhpoto/https/emoji.discadia.com/emojis/798267a5-3e6c-4168-a56c-9fc4be65aad3.gif')
+    
+    .setAssetsLargeText('Team MGC')
+    .addButton('Connect To Server', 'https://dsc.gg/kurukkanmoola')
     .addButton('CHROME', 'https://google.com');
-
+  
   client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+  client.user.setPresence({ status: "dnd" });
+})
 
-  let prevTime = null;
-  setInterval(() => {
-    const newTime = formatTime();
-    if (newTime !== prevTime) {
-      const newDetails = `Blood Bleeding  , KidneY 🍒 [${newTime}]`;
-      r.setDetails(newDetails);
-      client.user.setActivity(r);
-      prevTime = newTime;
-    }
-  }, 1000); // Update every second
-});
+client.login(process.env.TOKEN)
 
-const mySecret = process.env['TOKEN'];
-client.login(mySecret);
+// Join discord.gg/CyberSec for support! //
